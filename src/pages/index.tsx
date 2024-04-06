38e694e6df76dd444df217b959c5ccf4fbedc8fc
Fulton Browne <fultonbrowne@pm.me>
2024-04-06 12:49:49 -0500

    added all the sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,117 +1,273 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import React from "react";
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+import { Lora } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const lora = Lora({
+  subsets: ["latin"],
+  weight: "700",
+});
 
 export default function Home() {
   return (
+    // Enable vertical snap scrolling on the main container
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`${lora.className} h-screen snap-y snap-mandatory overflow-y-scroll`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      {/* Section 0 with snap alignment */}
+      <div
+        className="relative flex min-h-screen snap-start items-center p-4 md:justify-start md:p-24"
+        id="0"
+      >
+        <div className="w-full md:w-1/3">
+          <div className="text-6xl md:text-9xl">
+            Open
+            <br />
+            Health
+            <br />
+            Data
+          </div>
+          <div className="mt-4 text-xl">
+            The case for a more open, patient oriented health data system
+          </div>
+          <div className="mt-4 text-lg italic">By Fulton Browne</div>
+        </div>
+        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
+          <FaArrowDown
+            className="animate-bounce text-3xl"
+            onClick={() => {
+              document
+                .getElementById("1")
+                ?.scrollIntoView({ behavior: "smooth" });
+            }}
+          />
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* Section 1 with snap alignment */}
+      <div
+        className="relative flex min-h-screen w-full snap-start content-center justify-center p-4"
+        id="1"
+      >
+        <div className="flex flex-col content-center justify-center gap-4 md:gap-16">
+          <div className="w-full justify-center text-5xl md:text-7xl">
+            The modern world is built on data
+          </div>
+        </div>
+        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
+          <FaArrowDown
+            className="animate-bounce text-3xl"
+            onClick={() => {
+              document
+                .getElementById("2")
+                ?.scrollIntoView({ behavior: "smooth" });
+            }}
+          />
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {/* Section 2 with snap alignment */}
+      <div
+        className="relative flex min-h-screen w-full snap-start content-center justify-center p-4"
+        id="2"
+      >
+        <div className="flex flex-col content-center justify-center gap-4 md:gap-16">
+          <div className="w-full justify-center text-center text-2xl italic">
+            Building a healthcare data access system that prioritizes the
+            patient is a necessary step in bringing the healthcare system fully
+            into the 21st century.
+          </div>
+        </div>
+        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
+          <FaArrowDown
+            className="animate-bounce text-3xl"
+            onClick={() => {
+              document
+                .getElementById("3")
+                ?.scrollIntoView({ behavior: "smooth" });
+            }}
+          />
+        </div>
+      </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {/* Section 3 with snap alignment */}
+      <div
+        className="relative flex min-h-screen w-full snap-start content-center justify-center p-4"
+        id="3"
+      >
+        <div className="flex flex-col content-center justify-center gap-4 md:gap-16">
+          <div className="w-full justify-center text-center text-2xl italic"></div>
+        </div>
+        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
+          <FaArrowDown
+            className="animate-bounce text-3xl"
+            onClick={() => {
+              document
+                .getElementById("4")
+                ?.scrollIntoView({ behavior: "smooth" });
+            }}
+          />
+        </div>
+      </div>
+      {/* Section 4 with snap alignment */}
+      <div
+        className="relative flex min-h-screen snap-start items-center p-4 md:justify-start md:p-24"
+        id="4"
+      >
+        {/* Content for section 4 */}
+        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
+          <FaArrowDown
+            className="animate-bounce text-3xl"
+            onClick={() => {
+              document
+                .getElementById("5")
+                ?.scrollIntoView({ behavior: "smooth" });
+            }}
+          />
+        </div>
+      </div>
+      {/* Section 5 with snap alignment */}
+      <div
+        className="relative flex min-h-screen snap-start items-center p-4 md:justify-start md:p-24"
+        id="5"
+      >
+        {/* Content for section 5 */}
+        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
+          <FaArrowDown
+            className="animate-bounce text-3xl"
+            onClick={() => {
+              document
+                .getElementById("6")
+                ?.scrollIntoView({ behavior: "smooth" });
+            }}
+          />
+        </div>
+      </div>
+      {/* Section 6 with snap alignment */}
+      <div
+        className="relative flex min-h-screen snap-start items-center p-4 md:justify-start md:p-24"
+        id="6"
+      >
+        {/* Content for section 6 */}
+        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
+          <FaArrowDown
+            className="animate-bounce text-3xl"
+            onClick={() => {
+              document
+                .getElementById("7")
+                ?.scrollIntoView({ behavior: "smooth" });
+            }}
+          />
+        </div>
+      </div>
+      {/* Section 7 with snap alignment */}
+      <div
+        className="relative flex min-h-screen snap-start items-center p-4 md:justify-start md:p-24"
+        id="7"
+      >
+        {/* Content for section 7 */}
+        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
+          <FaArrowDown
+            className="animate-bounce text-3xl"
+            onClick={() => {
+              document
+                .getElementById("8")
+                ?.scrollIntoView({ behavior: "smooth" });
+            }}
+          />
+        </div>
+      </div>
+      {/* Section 8 with snap alignment */}
+      <div
+        className="relative flex min-h-screen snap-start items-center p-4 md:justify-start md:p-24"
+        id="8"
+      >
+        {/* Content for section 8 */}
+        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
+          <FaArrowDown
+            className="animate-bounce text-3xl"
+            onClick={() => {
+              document
+                .getElementById("9")
+                ?.scrollIntoView({ behavior: "smooth" });
+            }}
+          />
+        </div>
+      </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
+      {/* Section 9 with snap alignment */}
+      <div
+        className="relative flex min-h-screen snap-start items-center p-4 md:justify-start md:p-24"
+        id="9"
+      >
+        {/* Content for section 9 */}
+        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
+          <FaArrowDown
+            className="animate-bounce text-3xl"
+            onClick={() => {
+              document
+                .getElementById("10")
+                ?.scrollIntoView({ behavior: "smooth" });
+            }}
+          />
+        </div>
+      </div>
+      {/* Section 10 with snap alignment */}
+      <div
+        className="relative flex min-h-screen snap-start items-center p-4 md:justify-start md:p-24"
+        id="10"
+      >
+        {/* Content for section 10 */}
+        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
+          <FaArrowDown
+            className="animate-bounce text-3xl"
+            onClick={() => {
+              document
+                .getElementById("11")
+                ?.scrollIntoView({ behavior: "smooth" });
+            }}
+          />
+        </div>
+      </div>
+      {/* Section 11 with snap alignment */}
+      <div
+        className="relative flex min-h-screen snap-start items-center p-4 md:justify-start md:p-24"
+        id="11"
+      >
+        {/* Content for section 11 */}
+        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
+          <FaArrowDown
+            className="animate-bounce text-3xl"
+            onClick={() => {
+              document
+                .getElementById("12")
+                ?.scrollIntoView({ behavior: "smooth" });
+            }}
+          />
+        </div>
+      </div>
+      {/* Section 12 with snap alignment */}
+      <div
+        className="relative flex min-h-screen snap-start items-center p-4 md:justify-center md:p-24"
+        id="12"
+      >
+        {/* Content for section 12  just a "thanks for reading"*/}
+        <div>
+          <h1 className="text-center text-6xl">Thanks for reading!</h1>
+          <p className="text-1xl text-center italic">
+            Fulton Browne - Seminar 102 - 2024
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
+        {/* arrow up to top */}
+        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 transform">
+          <FaArrowUp
+            className="animate-bounce text-3xl"
+            onClick={() => {
+              document
+                .getElementById("0")
+                ?.scrollIntoView({ behavior: "smooth" });
+            }}
+          />
+        </div>
       </div>
     </main>
   );
